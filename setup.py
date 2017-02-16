1979581aaeb9dfa3d4fed7fc0b389388a9cb43a0
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-16 11:57:46 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
 from distutils.core import setup
 setup(name='cbtest',
-      version='1.0',
+      version='0.0.2',
+      author='Peter Claydon',
+      author_email='peter.claydon@continuumbridge.com',
       py_modules=['cbtest'],
       )
