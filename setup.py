13ce169f64d89a42672fdb5c9db6a9df0a5688c9
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-16 12:28:38 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='cbtest',
-      version='0.0.2',
+      version='0.0.4',
       author='Peter Claydon',
       author_email='peter.claydon@continuumbridge.com',
       py_modules=['cbtest'],
+      entry_points={
+          'console_scripts': [
+              'httpstat = cbtest:main'
       )
