9771022afb575c635cc191d95419f2594d5395fc
Peter Claydon <peter.claydon@continuumbridge.com>
2017-02-17 09:55:18 +0000

    Testing

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(name='cbtest',
-      version='0.0.11',
+      version='0.0.12',
       author='Peter Claydon',
       author_email='peter.claydon@continuumbridge.com',
       py_modules=['cbtest'],
       install_requires=[
-          'Twisted'==16.4.1
+          'Twisted'
       ]
       entry_points={
           'console_scripts': [
